2dc5fcbc62b0d13eed7497933d17ac8b5ba22c24
avery-wang <wangavery0@gmail.com>
2026-04-20 19:07:02 -0400

    New random forest

--- RandomForest.R
+++ RandomForest.R
@@ -5,83 +5,18 @@ set.seed(123)
 
 # Hyperparameter grid
 grid <- expand.grid(
-  mtry = c(3, 4, 5, 6),
-  splitrule = c("variance"),
-  min.node.size = c(1, 5, 10, 25, 50)
+  mtry = c(3, 5),
+  splitrule = c("gini"),
+  min.node.size = c(5, 10, 25)
 )
-
-# Fit a model (no regularization)
-rf <- ranger(DEP_DEL15 ~ ., data = flights)
 
 # Fit cross validation on grid
 cv <- train(
-  DEP_DEL15 ~ .,
-  data = flights,
+  DEP_DEL15 ~ . - YEAR,
+  data = train_data,
   method = "ranger",
   tuneGrid = grid,
-  num.trees = 500,
-  importance = "permutation"
+  num.trees = 500
 )
 
-# Test model on visible 2025 flights
-flights2025_visible <- read_csv("data/flights2025_visible.csv") |>
-  
-  # Correct data types
-  mutate(
-    FL_DATE = parse_date_time(FL_DATE, orders = "mdY IMS p"),
-    FL_DATE = as.Date(FL_DATE),
-    
-    YEAR = as.factor(YEAR),
-    MONTH = as.factor(MONTH),
-    DAY_OF_WEEK = as.factor(DAY_OF_WEEK),
-    
-    OP_UNIQUE_CARRIER = as.factor(OP_UNIQUE_CARRIER),
-    DEST = as.factor(DEST),
-    DEST_STATE_ABR = as.factor(DEST_STATE_ABR),
-    
-    # DEP_DEL15 = as.factor(DEP_DEL15)
-  ) |>
-  
-  # Compute carrier day delay rate
-  filter(!is.na(DEP_DEL15)) %>%
-  arrange(FL_DATE, OP_UNIQUE_CARRIER, CRS_DEP_TIME) %>%
-  group_by(FL_DATE, OP_UNIQUE_CARRIER) %>%
-  mutate(
-    carrier_day_delay_rate =
-      lag(cumsum(DEP_DEL15), default = 0) /
-      pmax(row_number() - 1, 1),
-    n_flights_day = n()
-  ) %>%
-  mutate(
-    carrier_day_delay_rate = if_else(row_number() == 1, 0, carrier_day_delay_rate)
-  ) %>%
-  ungroup()
-
-# Join delay rates from 23-24, fill missing ones with 0
-carrier_dest_rates <- flights |>
-  select(OP_UNIQUE_CARRIER, DEST, carrier_route_delay_rate) |>
-  distinct()
-dest_rates <- flights |>
-  select(DEST, dest_delay_rate) |>
-  distinct()
-carrier_month_rates <- flights |>
-  select(OP_UNIQUE_CARRIER, MONTH, carrier_month_delay_rate) |>
-  distinct()
-carrier_rates <- flights |>
-  select(OP_UNIQUE_CARRIER, carrier_delay_rate) |>
-  distinct()
-
-flights2025_visible <- flights2025_visible |>
-  left_join(carrier_dest_rates, by = c("OP_UNIQUE_CARRIER", "DEST")) |>
-  left_join(dest_rates, by = "DEST") |>
-  left_join(carrier_month_rates, by = c("OP_UNIQUE_CARRIER", "MONTH")) |>
-  left_join(carrier_rates, by = "OP_UNIQUE_CARRIER") |>
-  mutate(across(ends_with("_delay_rate"), ~coalesce(.x, 0)))
-
-# Check if any NAs
-colSums(is.na(flights2025_visible))
-
-predictions_2025 <- predict(rf, data = flights2025_visible)
-
-# Brier score (MSE)
-mean((predictions_2025$predictions - flights2025_visible$DEP_DEL15)^2)
+library(pROC)
