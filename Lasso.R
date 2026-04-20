db2f8233d1b39fb4b956b9367ec00882725a62b1
Aishani Pradhan <aishani.pradhan2001@gmail.com>
2026-04-20 19:15:52 -0400

    LASSO implementation

---
+++ Lasso.R
@@ -0,0 +1,43 @@
+# First, run all cells in delay_prediction.qmd to load data
+set.seed(67)
+
+library(glmnet)
+library(pROC)
+
+# Create model matrices
+x_train <- model.matrix(DEP_DEL15 ~ . - YEAR, data = train_data)[, -1]
+y_train <- train_data$DEP_DEL15
+
+x_test <- model.matrix(DEP_DEL15 ~ . - YEAR, data = test_data)[, -1]
+
+# Fit lasso logistic regression with cross-validation
+cv_lasso <- cv.glmnet(
+  x_train,
+  y_train,
+  family = "binomial",
+  alpha = 1
+)
+
+# Best lambda
+best_lambda <- cv_lasso$lambda.min
+
+# Fit final lasso model
+lasso_model <- glmnet(
+  x_train,
+  y_train,
+  family = "binomial",
+  alpha = 1,
+  lambda = best_lambda
+)
+
+# Predicted probabilities on test set
+lasso_pred <- predict(
+  lasso_model,
+  newx = x_test,
+  type = "response"
+)
+
+# AUC
+auc(
+  roc(test_data$DEP_DEL15, as.numeric(lasso_pred))
+)
